7894e579465cb6372e8ff0779dbbf79a8744d777
Edward A Maxedon <edadma@gmail.com>
2018-04-20 14:28:43 -0400

    cont coding filter unit testing

--- src/test/scala/FilterTests.scala
+++ src/test/scala/FilterTests.scala
@@ -21,6 +21,58 @@ class FilterTests extends FreeSpec with PropertyChecks with Matchers with Testin
 			""".stripMargin
 	}
 
+	"append" in {
+		test(
+			"""
+				|{{ "/my/fancy/url" | append: ".html" }}
+				|{{ "website.com" | append: filename }}
+			""".stripMargin
+		) shouldBe
+			"""
+				|/my/fancy/url.html
+				|website.com/index.html
+			""".stripMargin
+	}
+
+	"at_least" in {
+		test(
+			"""
+				|{{ 4 | at_least: 5 }}
+				|{{ 4 | at_least: 3 }}
+			""".stripMargin
+		) shouldBe
+			"""
+				|5
+				|4
+			""".stripMargin
+	}
+
+	"at_most" in {
+		test(
+			"""
+				|{{ 4 | at_most: 5 }}
+				|{{ 4 | at_most: 3 }}
+			""".stripMargin
+		) shouldBe
+			"""
+				|4
+				|3
+			""".stripMargin
+	}
+
+	"capitalize" in {
+		test(
+			"""
+				|{{ "title" | capitalize }}
+				|{{ "my great title" | capitalize }}
+			""".stripMargin
+		) shouldBe
+			"""
+				|Title
+				|My great title
+			""".stripMargin
+	}
+
 	"sort_natural" in {
 		test( """{{ "zebra, octopus, giraffe, 2, 1, Sally Snake" | split: ", " | sort_natural | join: ", " }}""" ) shouldBe
 			"1, 2, giraffe, octopus, Sally Snake, zebra"
