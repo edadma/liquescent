88bb7698f39336b4e4ffb9b053d34813cd8d1d5b
Edward A Maxedon <edadma@gmail.com>
2018-04-29 20:05:05 -0400

    code 'money' filter'

--- src/main/scala/ExtraMoneyFilters.scala
+++ src/main/scala/ExtraMoneyFilters.scala
@@ -10,14 +10,17 @@ object ExtraMoneyFilters {
       new Filter( "money" ) {
         override def parameters = List( List(NumberType) )
 
-        override def apply( settings: Map[String, Any], args: List[Any] ) =
+        override def apply( settings: Map[String, Any], args: List[Any] ) = {
+          val (places: Int, format: String) = settings("currency-money")
+
+          def money( m: BigDecimal ) = format.format( m )
+
           args match {
-            case List( n: Int ) => money( BigDecimal(n) )
-            case List( n: BigInt ) => money( BigDecimal(n) )
+            case List( n: Int ) => money( BigDecimal(n)/BigDecimal(10).pow(places) )
+            case List( n: BigInt ) => money( BigDecimal(n)/BigDecimal(10).pow(places) )
             case List( n: BigDecimal ) => money( n )
           }
-
-        def money( m: BigDecimal ) = ""
+        }
       }
 
     ) map {f => (f.name, f)} toMap
