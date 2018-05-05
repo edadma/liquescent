752f343a9131fd83bb3932a09267929a668310a4
Edward A Maxedon Sr <edadma@gmail.com>
2018-05-05 17:24:30 -0400

    code 'img_tag' filter

--- src/main/scala/ExtraHTMLFilters.scala
+++ src/main/scala/ExtraHTMLFilters.scala
@@ -7,6 +7,17 @@ object ExtraHTMLFilters {
   val map =
     List(
 
+      new Filter( "img_tag" ) {
+        override def parameters = List( List(StringType), List(StringType, StringType), List(StringType, StringType, StringType) )
+
+        override def apply( interp: Interpreter, settings: Map[Symbol, Any], args: List[Any], named: Map[String, Any], locals: Map[String, Any] ) =
+          args match {
+            case List( src: String ) => s"""<img src="$src" />"""
+            case List( src: String, alt: String ) => s"""<img src="$src" alt="$alt" />"""
+            case List( src: String, alt: String, cls: String ) => s"""<img src="$src" alt="$alt" class="$cls" />"""
+          }
+      },
+
       new Filter( "script_tag" ) {
         override def parameters = List( List(StringType) )
 
