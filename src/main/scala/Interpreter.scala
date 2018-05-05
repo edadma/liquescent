c70a0aef1aa64585ea938f577e95d3269d316af2
Edward A Maxedon <edadma@gmail.com>
2018-05-05 00:09:46 -0400

    start coding local variable support

--- src/main/scala/Interpreter.scala
+++ src/main/scala/Interpreter.scala
@@ -24,20 +24,24 @@ class Interpreter( filters: Map[String, Filter], tags: Map[String, Tag], setting
 			case Some( scope ) => scope(name) = value
 		}
 
-	def getVar( name: String ) =
+	def getVar( name: String, locals: Map[String, String] ) =
 		scopes.view.reverse find (_ contains name) match {
 			case None =>
-				vars get name match {
-					case None => nil
-					case Some( v ) => v
-				}
+        locals get name match {
+          case None =>
+            vars get name match {
+              case None => nil
+              case Some( v ) => v
+            }
+          case Some( v ) => v
+        }
 			case Some( scope ) => scope(name)
 		}
 
-  def capture( statement: StatementAST ) = {
+  def capture( statement: StatementAST, locals: Map[String, String] ) = {
     val bytes = new ByteArrayOutputStream
 
-    execute( statement, new PrintStream(bytes) )
+    execute( statement, locals, new PrintStream(bytes) )
     bytes.toString
   }
 
@@ -45,35 +49,35 @@ class Interpreter( filters: Map[String, Filter], tags: Map[String, Tag], setting
 
 	def exitScope: Unit = scopes remove scopes.length - 1
 
-  def render( parse: ParseResult, out: PrintStream, dolayout: Boolean ): Unit = {
+  def render( parse: ParseResult, locals: Map[String, String], out: PrintStream, dolayout: Boolean ): Unit = {
     if (dolayout && parse.layout.nonEmpty) {
-      setVar( "content_for_layout", capture(parse.statement) )
+      setVar( "content_for_layout", capture(parse.statement, locals) )
 
       val file = new File( docroot("layout", settings), parse.layout.get + ".liquid" )
 
       if (parse.layout.get == "theme" && file.exists && file.isFile && file.canRead || parse.layout.get != "theme")
-        include( file, out )
+        include( file, locals, out )
       else
-        execute( parse.statement, out )
+        execute( parse.statement, locals, out )
     } else
-      execute( parse.statement, out )
+      execute( parse.statement, locals, out )
   }
 
-  def include( input: File, out: PrintStream ) =
-    execute( LiquescentParser.parse(io.Source.fromFile(input)).statement, out )
+  def include( input: File, locals: Map[String, String], out: PrintStream ) =
+    execute( LiquescentParser.parse(io.Source.fromFile(input)).statement, locals, out )
 
-  def execute( op: StatementAST, out: PrintStream ): Unit = {
+  def execute( op: StatementAST, locals: Map[String, String], out: PrintStream ): Unit = {
     op match {
       case LayoutStatementAST( _ ) =>
       case PlainOutputStatementAST( s ) => out.print( s )
       case ExpressionOutputStatementAST( expr ) =>
         out.print(
-					eval( expr ) match {
+					eval( expr, locals ) match {
             case l: List[_] => l map display mkString
             case s => display( s )
           }
 				)
-			case AssignStatementAST( name, expr ) => setVar( name, eval(expr) )
+			case AssignStatementAST( name, expr ) => setVar( name, eval(expr, locals) )
 			case IncrementStatementAST( name ) =>
 				out.print( incdec get name match {
 					case None =>
@@ -99,42 +103,42 @@ class Interpreter( filters: Map[String, Filter], tags: Map[String, Tag], setting
 			case CustomTagStatementAST( name, args ) =>
 				tags get name match {
 					case None => sys.error( s"unknown tag: $name" )
-					case Some( t ) => t( settings, vars, out, args map eval, context )
+					case Some( t ) => t( settings, vars, out, args map (a => eval(a, locals)), context )
 				}
-			case BlockStatementAST( block ) => block foreach (execute( _, out ))
+			case BlockStatementAST( block ) => block foreach (execute( _, locals, out ))
 			case IfStatementAST( cond, els ) =>
-				cond find { case (expr, _) => truthy( eval(expr) ) } match {
+				cond find { case (expr, _) => truthy( eval(expr, locals) ) } match {
 					case None =>
 						els match {
 							case None =>
-							case Some( elseStatement ) => execute( elseStatement, out )
+							case Some( elseStatement ) => execute( elseStatement, locals, out )
 						}
-					case Some( (_, thenStatement) ) => execute( thenStatement, out )
+					case Some( (_, thenStatement) ) => execute( thenStatement, locals, out )
 				}
 			case CaseStatementAST( exp, cases, els ) =>
-				val value = eval( exp )
+				val value = eval( exp, locals )
 
-				cases find { case (expr, _) => eval( expr ) == value } match {
+				cases find { case (expr, _) => eval( expr, locals ) == value } match {
 					case None =>
 						els match {
 							case None =>
-							case Some( elseStatement ) => execute( elseStatement, out )
+							case Some( elseStatement ) => execute( elseStatement, locals, out )
 						}
-					case Some( (_, whenStatement) ) => execute( whenStatement, out )
+					case Some( (_, whenStatement) ) => execute( whenStatement, locals, out )
 				}
 			case UnlessStatementAST( cond, els ) =>
-				cond find { case (expr, _) => falsy( eval(expr) ) } match {
+				cond find { case (expr, _) => falsy( eval(expr, locals) ) } match {
 					case None =>
 						els match {
 							case None =>
-							case Some( elseStatement ) => execute( elseStatement, out )
+							case Some( elseStatement ) => execute( elseStatement, locals, out )
 						}
-					case Some( (_, thenStatement) ) => execute( thenStatement, out )
+					case Some( (_, thenStatement) ) => execute( thenStatement, locals, out )
 				}
-			case CaptureStatementAST( name, body ) => setVar( name, capture(body) )
+			case CaptureStatementAST( name, body ) => setVar( name, capture(body, locals) )
 			case ForStatementAST( name, expr, parameters, body ) =>
 				var list =
-					eval( expr ) match {
+					eval( expr, locals ) match {
 						case s: Seq[_] => s
 						case x => sys.error( s"expected array: $x" )
 					}
@@ -142,12 +146,12 @@ class Interpreter( filters: Map[String, Filter], tags: Map[String, Tag], setting
 				parameters foreach {
 					case ReversedForParameter => list = list.reverse
 					case LimitForParameter( limit ) =>
-						eval( limit ) match {
+						eval( limit, locals ) match {
 							case n: Number => list = list take n.intValue
 							case v => sys.error( s"number was expected: $v" )
 						}
 					case OffsetForParameter( offset ) =>
-						eval( offset ) match {
+						eval( offset, locals ) match {
 							case n: Number => list = list drop n.intValue
 							case v => sys.error( s"number was expected: $v" )
 						}
@@ -160,7 +164,7 @@ class Interpreter( filters: Map[String, Filter], tags: Map[String, Tag], setting
 						try {
 							setVar( name, elem )
 							setVar( "#idx", idx )
-							execute( body, out )
+							execute( body, locals, out )
 						} catch {
 							case _: ContinueException =>
 						}
@@ -169,7 +173,7 @@ class Interpreter( filters: Map[String, Filter], tags: Map[String, Tag], setting
 				}
 
 				exitScope
-			case CycleStatementAST( items ) => out.print( display(eval(items(getVar("#idx").asInstanceOf[Int]%items.length))) )
+			case CycleStatementAST( items ) => out.print( display(eval(items(getVar("#idx", locals).asInstanceOf[Int]%items.length), locals)) )
 			case BreakStatementAST => throw new BreakException
 			case ContinueStatementAST => throw new ContinueException
     }
@@ -214,23 +218,23 @@ class Interpreter( filters: Map[String, Filter], tags: Map[String, Tag], setting
 			case _ => sys.error( s"expected two numbers or two strings: $a, $b" )
 		}
 
-  def eval( expr: ExpressionAST ): Any =
+  def eval( expr: ExpressionAST, locals: Map[String, String] ): Any =
     expr match {
 			case RangeExpressionAST( from, to ) =>
-				((eval( from ), eval( to )) match {
+				((eval( from, locals ), eval( to, locals )) match {
 					case (a: Int, b: Int) => a to b
 					case (a: BigDecimal, b: BigDecimal) => a to b by 1
 					case (a: BigInt, b: BigInt) => a to b by 1
 					case (a, b) => sys.error( s"invalid range limits: $a, $b" )
 				}) toList
       case ArrayExpressionAST( exp, index ) =>
-        (eval( exp ), eval( index )) match {
+        (eval( exp, locals ), eval( index, locals )) match {
           case (m: collection.Map[_, _], i: String) => m.asInstanceOf[collection.Map[String, Any]](i)
           case (s: Seq[_], i: Number) => s.asInstanceOf[Seq[Any]]( i.intValue )
           case (a, b) => sys.error( s"bracket syntax not applicable: $a, $b" )
         }
       case DotExpressionAST( exp, name ) =>
-        eval( exp ) match {
+        eval( exp, locals ) match {
           case `nil` => nil
           case m: collection.Map[_, _] =>
             m.asInstanceOf[collection.Map[String, Any]] get name match {
@@ -251,22 +255,22 @@ class Interpreter( filters: Map[String, Filter], tags: Map[String, Tag], setting
         filters get name match {
           case None => sys.error( s"unknown filter: $name" )
           case Some( f ) =>
-            applyFilter( eval(operand), f,
-              args filter (_._1 == null) map {case (_, v) => eval(v)},
-              args filterNot (_._1 == null) map {case (k, v) => (k, eval(v))} toMap)
+            applyFilter( eval(operand, locals), f,
+              args filter (_._1 == null) map {case (_, v) => eval(v, locals)},
+              args filterNot (_._1 == null) map {case (k, v) => (k, eval(v, locals))} toMap)
         }
       case LiteralExpressionAST( o ) => o
-      case VariableExpressionAST( name ) => getVar( name )
-			case OrExpressionAST( left, right ) => truthy( eval(left) ) || truthy( eval(right) )
-			case AndExpressionAST( left, right ) => truthy( eval(left) ) && truthy( eval(right) )
-			case EqExpressionAST( left, right ) => eval( left ) == eval( right )
-			case NeqExpressionAST( left, right ) => eval( left ) != eval( right )
-			case LtExpressionAST( left, right ) => compare( eval(left), eval(right) ) < 0
-			case LteExpressionAST( left, right ) => compare( eval(left), eval(right) ) <= 0
-			case GtExpressionAST( left, right ) => compare( eval(left), eval(right) ) > 0
-			case GteExpressionAST( left, right ) => compare( eval(left), eval(right) ) >= 0
+      case VariableExpressionAST( name ) => getVar( name, locals )
+			case OrExpressionAST( left, right ) => truthy( eval(left, locals) ) || truthy( eval(right, locals) )
+			case AndExpressionAST( left, right ) => truthy( eval(left, locals) ) && truthy( eval(right, locals) )
+			case EqExpressionAST( left, right ) => eval( left, locals ) == eval( right, locals )
+			case NeqExpressionAST( left, right ) => eval( left, locals ) != eval( right, locals )
+			case LtExpressionAST( left, right ) => compare( eval(left, locals), eval(right, locals) ) < 0
+			case LteExpressionAST( left, right ) => compare( eval(left, locals), eval(right, locals) ) <= 0
+			case GtExpressionAST( left, right ) => compare( eval(left, locals), eval(right, locals) ) > 0
+			case GteExpressionAST( left, right ) => compare( eval(left, locals), eval(right, locals) ) >= 0
 			case ContainsExpressionAST( left, right ) =>
-				(eval( left ), eval( right )) match {
+				(eval( left, locals ), eval( right, locals )) match {
 					case (seq: Seq[_], str: String) => seq contains str
 					case (s1: String, s2: String) => s1 contains s2
 					case (a, b) => sys.error( s"expected string/array contains string: $a, $b" )
