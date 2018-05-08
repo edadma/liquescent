c8d2ac1384363bf512c6003ff0f1734d06a32d19
Edward A Maxedon Sr <edadma@gmail.com>
2018-05-07 20:21:27 -0400

    bump version to 0.2_snapshot_6

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -15,9 +15,9 @@ object Main extends App {
 
 	def usage {
 		"""
-			|liquescent v0.2_snapshot_5.1
+			|liquescent v0.2_snapshot_6
 			|
-			|Usage:  java -jar liquescent-0.2_snapshot_5.1.jar <options> <liquid template>
+			|Usage:  java -jar liquescent-0.2_snapshot_6.jar <options> <liquid template>
 			|
 			|Options:  --help              display this help and exit
 			|          -s <name> <string>  assign <string> to variable <name>
