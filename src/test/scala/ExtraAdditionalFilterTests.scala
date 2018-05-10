55a6d6a6307a80ba8863fc52eada43d813f88eb9
Edward A Maxedon Sr <edadma@gmail.com>
2018-05-10 17:05:02 -0400

    fix 'time_tag' filter

--- src/test/scala/ExtraAdditionalFilterTests.scala
+++ src/test/scala/ExtraAdditionalFilterTests.scala
@@ -12,7 +12,7 @@ class ExtraAdditionalFilterTests extends FreeSpec with PropertyChecks with Match
   "time_tag" in {
 		test( "{{ time | time_tag }}", true,
       "time" -> OffsetDateTime.parse("2016-02-24T09:47:51-05:00") ) shouldBe
-      """<time datetime="2016-02-24T14:47:51Z">Wed, 24 Feb 2016 09:47:51 -05:00</time>"""
+      """<time datetime="2016-02-24T14:47:51Z">Wed, 24 Feb 2016 09:47:51 -0500</time>"""
 		test( "{{ time | time_tag: '%b %d, %Y' }}", true,
       "time" -> OffsetDateTime.parse("2016-02-24T09:47:51-05:00") ) shouldBe
       """<time datetime="2016-02-24T14:47:51Z">Feb 24, 2016</time>"""
