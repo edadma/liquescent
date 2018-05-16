ba9f3403f60d84805cd9c6592c74a8acc8b6758d
Edward A. Maxedon, Sr <edadma@gmail.com>
2018-05-15 22:10:35 -0400

    bump version to 0.2

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "liquescent"
 
-version := "0.2_snapshot_9"
+version := "0.2"
 
 scalaVersion := "2.12.6"
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -15,9 +15,9 @@ object Main extends App {
 
 	def usage {
 		"""
-			|liquescent v0.2_snapshot_9
+			|liquescent v0.2
 			|
-			|Usage:  java -jar liquescent-0.2_snapshot_9.jar <options> <liquid template>
+			|Usage:  java -jar liquescent-0.2.jar <options> <liquid template>
 			|
 			|Options:  --help              display this help and exit
 			|          -s <name> <string>  assign <string> to variable <name>
